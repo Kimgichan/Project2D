ec9761cd77decbee76598db0d504376d57de034e
DESKTOP-PC889I8\rlcks <gichan@vr-jamong.com>
2021-12-29 09:04:19 +0900

    조이스틱 스크립트 작업 중 InputField 쪽

--- unityproject/Assets/Joystick/Scripts/Joystick.cs
+++ unityproject/Assets/Joystick/Scripts/Joystick.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Joystick : MonoBehaviour
 {
+    //차일드 0 -> InputField; 1 -> BG;
+    //BG의 차일드 0 -> Handle;
+    EventTrigger eventTrigger;
+    GameObject inputField;
+    GameObject bg;
+    GameObject handle;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        inputField = transform.GetChild(0).gameObject;
+        bg = transform.GetChild(1).gameObject;
+        handle = transform.GetChild(1).GetChild(0).gameObject;
 
-    // Update is called once per frame
-    void Update()
-    {
+        eventTrigger = inputField.AddComponent<EventTrigger>();
+
+        var entry_PointerDown = new EventTrigger.Entry();
+        entry_PointerDown.eventID = EventTriggerType.PointerDown;
+        entry_PointerDown.callback.AddListener(e => {
         
+        });
+        eventTrigger.triggers.Add(entry_PointerDown);
+
+
     }
 }
