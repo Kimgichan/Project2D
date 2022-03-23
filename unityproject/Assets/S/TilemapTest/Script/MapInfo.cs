a0dc98c8de148ca61a16ef619759049facd4ab6c
AppleMangoSpd <52735083+AppleMangoSpd@users.noreply.github.com>
2022-03-23 20:18:24 +0900

    데모 숲 지역 추가 -정비->사냥1->사냥2->정산 스테이지로 구성 -카메라가 플레이어를 따라가지 않는 버그 있음 S/TilemapTest/Scene/DemoMapScene/DemoSceneFromU 씬 파일 참고

--- unityproject/Assets/S/TilemapTest/Script/MapInfo.cs
+++ unityproject/Assets/S/TilemapTest/Script/MapInfo.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class MapInfo : MonoBehaviour
 {
+    public enum MapState { Quest, Hunt, Result };
+
     [Header("Enterance")]
     public GameObject enterance;
+    [Header("Exit")]
     public GameObject exit;
+    [Header("Current Map")]
+    public MapState currentMapState;
 }
