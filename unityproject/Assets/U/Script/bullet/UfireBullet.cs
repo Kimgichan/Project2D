f93bed7c3acb6e64513a6d83336ca11735075936
DESKTOP-C24N2G4\유성현 <dbtjdgus@gmail.com>
2022-03-31 11:03:20 +0900

    1. 프리팹 fire에 트레일 장착 테스트 백업용

---
+++ unityproject/Assets/U/Script/bullet/UfireBullet.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UfireBullet : MonoBehaviour
+{
+    public float speed = 0.2f;
+    public float damage = 10.0f;
+    void Start()
+    {
+        Invoke("DestroyBullet", 2);
+    }
+
+
+    void Update()
+    {
+        transform.Translate(transform.up * speed * Time.deltaTime);
+    }
+
+    void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnTriggerStay2D(Collider2D _other)
+    {
+        if (_other.gameObject.tag == "EnemyHitBox")
+        {
+            _other.GetComponent<UHitCollider>().myEnemy.hitHP(damage);
+        }
+    }
+}
