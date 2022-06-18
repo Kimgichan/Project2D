2a9a78367ff6c33107bc8f39a0125f1c8af1a655
AppleMangoSpd <52735083+AppleMangoSpd@users.noreply.github.com>
2022-06-18 12:33:54 +0900

    뿌리공격 구현중

--- unityproject/Assets/S/BossTest/RootAttack.cs
+++ unityproject/Assets/S/BossTest/RootAttack.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class RootAttack : MonoBehaviour
 {
+    public GameObject pre;
+    public GameObject main;
+
     public float lifeTime = 5.0f;
+    public float predictTime = 3.0f;
     private void Start()
     {
+        pre.SetActive(true);
+        main.SetActive(false);
+        StartCoroutine("AttackAfterSeconds");
+    }
+    private void StartAttack()
+    {
+        Debug.Log("StartAttack called");
+        main.SetActive(true);
         Destroy(this.gameObject, lifeTime);
     }
-
+    IEnumerator AttackAfterSeconds()
+    {
+        yield return new WaitForSeconds(predictTime);
+        pre.SetActive(false);
+        StartAttack();
+    }
 }
